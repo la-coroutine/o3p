497f03707cc5259fb551cfc6ac0fbb70cb2f81ec
Guillaume Libersat <glibersat@sigill.org>
2013-02-02 19:12:10 +0100

    add products app

---
+++ products/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import Invest, Usage
+
+admin.site.register(Invest)
+admin.site.register(Usage)
---
+++ products/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+from shop.models import Product
+from collect.models import Collect
+
+class Usage(Product):
+    class Meta:
+        pass
+
+class Invest(Product):
+    class Meta:
+        pass
+
